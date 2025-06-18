416bf3f316c67f4df2dc9e2852b90fc7f0c551a1
Bilkis-Mo-Yunus <siddiqui1998bilkis@gmail.com>
2025-06-18 15:54:58 +0530

    host allowed

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+35b8*bczdx19ph#gzb#f%vhbo^bme$*no*_uiaaeu(_v^)(io
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sana786.pythonanywhere.com']
 
 
 # Application definition
